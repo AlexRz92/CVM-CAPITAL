8ce4440cebd9321a816881a4a474628a1ae595d0
TheAlex <143757165+AlexRz92@users.noreply.github.com>
2025-08-13 16:50:57 -0400

    Update supabase.ts

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://omouzqcsylvmswyipcef.supabase.co';
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tb3V6cWNzeWx2bXN3eWlwY2VmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQyNjIyMzcsImV4cCI6MjA2OTgzODIzN30.TF3A1iMJpsP0pNa7uCZThtJl_IKjyurdVd7rpNT_lZ0';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://qcyoriwwzysgxjcwtyxa.supabase.co';
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjeW9yaXd3enlzZ3hqY3d0eXhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5NDMyOTEsImV4cCI6MjA2ODUxOTI5MX0.e6FNvKtPJUpkVFjDw4U0UhHtCG5Ko_ud0U7zzzTmk8A';
  
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
